1a7c02f47017c2e83a00ce2698dc57404506202b
Devi <diptikadevi@gmail.com>
2024-08-04 21:34:50 -0400

    implemented everything but sort

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -1,7 +1,160 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import java.util.Arrays;
+
 /**
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedList {
+
+    Node[] singlyList;
+
+    public SinglyLinkedList(Node[] list){
+        singlyList = list;
+    }
+
+    public SinglyLinkedList(){
+        singlyList = new Node[0];
+    }
+
+    public void add(Integer addValue){
+        if (singlyList.length == 0){
+            Node head = new Node(addValue, null, null);
+            updateAddArray(head);
+        } else {
+            Node previousLastNode = singlyList[singlyList.length-1];
+            Node newTail = new Node(addValue, previousLastNode, null);
+            previousLastNode.setNextNode(newTail);
+            updateAddArray(newTail);
+        }
+    }
+
+    public boolean remove(int i) {
+        try {
+            if (singlyList.length == 0 || singlyList[i] == null || i > singlyList.length - 1) {
+                return false;
+            }
+        } catch(ArrayIndexOutOfBoundsException e) {
+            System.out.println(e + "index out of scope");
+            return false;
+        }
+            Node newTail = singlyList[i-1];
+            newTail.setNextNode(null);
+            singlyList = Arrays.copyOf(singlyList, singlyList.length - 1);
+            singlyList[singlyList.length-1] = newTail;
+            return true;
+
+    }
+
+    public void updateAddArray(Node nodeToAdd) {
+        singlyList = Arrays.copyOf(singlyList, singlyList.length + 1);
+        singlyList[singlyList.length-1] = nodeToAdd;
+    }
+
+    public Integer get(Integer index) {
+        return singlyList[index].nodeValue;
+    }
+
+    public Boolean contains(Integer nodeValue){
+        for (Node element : singlyList){
+            if (element.getNodeValue().equals(nodeValue)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Integer find(Integer nodeValue){
+        for (int i = 0; i < singlyList.length; i++){
+            if (singlyList[i].getNodeValue().equals(nodeValue)){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public Integer size() {
+        return singlyList.length;
+    }
+
+    public SinglyLinkedList copy() {
+        SinglyLinkedList list = new SinglyLinkedList(Arrays.copyOf(singlyList, singlyList.length));
+        return list;
+
+    }
+
+    public SinglyLinkedList sort() {
+        SinglyLinkedList newList = new SinglyLinkedList();
+        newList = copy();
+
+
+
+        for (int i =0; i < singlyList.length; i++){
+            Integer minValue =Integer.MAX_VALUE;
+            for (int j = 0; j < singlyList.length; j++){
+            if (singlyList[j].nodeValue > singlyList[i].nodeValue && singlyList[j].nodeValue < minValue) {
+                minValue = singlyList[j].nodeValue;
+            }
+
+            }
+            if (singlyList[i].nodeValue.equals(minValue)){
+                newList.add(singlyList[i].nodeValue);
+            }
+
+        }
+        return newList;
+    }
+
+//    - add -- add an element to the list
+//	- remove -- remove an element (specified by numeric index) from the list
+//	- contains -- returns true if the element is in the list, false otherwise
+//	- find -- returns the element's index if it is in the list, -1 otherwise
+//	- size -- returns the current size of the list
+//	- get -- returns the element at the specified index
+//	- copy -- returns a new linked list containing the same values (look up deep versus shallow copy)
+//	- sort -- sorts the list using your algorithm of choice. You must perform the sorting yourself (no fair using someone else's library)
+
+
+    public class Node {
+
+        Integer nodeValue;
+        Node nextNode;
+        Node prevNode;
+
+        public Node(Integer newNodeValue, Node newPrevNode, Node newNextNode){
+            nodeValue = newNodeValue;
+            prevNode = newPrevNode;
+            nextNode = newNextNode;
+        }
+
+        public void setNextNode(Node nextNode) {
+            this.nextNode = nextNode;
+        }
+
+        public Integer getNodeValue() {
+            return nodeValue;
+        }
+
+        public void setPrevNode(Node prevNode) {
+            this.prevNode = prevNode;
+        }
+
+        public boolean isHead(){
+            if (prevNode == null){
+                return true;
+            }
+            return false;
+        }
+
+        public boolean isTail(){
+            if (nextNode == null){
+                return true;
+            }
+            return false;
+        }
+
+    }
+
 }
+
+
--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -1,7 +1,144 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedListTest {
+
+    @Test
+    public void addTest() {
+        SinglyLinkedList list = new SinglyLinkedList();
+        Integer expected = 4;
+        list.add(4);
+        Integer actual = list.get(0);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void addMultipleTest() {
+        SinglyLinkedList list = new SinglyLinkedList();
+        Integer expected = 8;
+        list.add(4);
+        list.add(8);
+        Integer actual = list.get(1);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void removeTest(){
+        SinglyLinkedList list = new SinglyLinkedList();
+        Integer expected = 1;
+        list.add(4);
+        list.add(8);
+        list.remove(1);
+        Integer actual = list.size();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void removeMultipleTest(){
+        SinglyLinkedList list = new SinglyLinkedList();
+        Integer expected = 1;
+        list.add(4);
+        list.add(8);
+        list.add(9);
+        list.remove(2);
+        list.remove(1);
+        Integer actual = list.size();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void removeTrueTest(){
+        SinglyLinkedList list = new SinglyLinkedList();
+        list.add(4);
+        list.add(8);
+        list.add(9);
+        list.remove(2);
+        Assert.assertTrue(list.remove(1));
+    }
+
+    @Test
+    public void removeFalseTest(){
+        SinglyLinkedList list = new SinglyLinkedList();
+        list.add(4);
+        list.add(8);
+        list.add(9);
+        Assert.assertFalse(list.remove(3));
+    }
+
+    @Test
+    public void containsTest(){
+        SinglyLinkedList list = new SinglyLinkedList();
+        list.add(4);
+        list.add(8);
+        list.add(9);
+        Assert.assertTrue(list.contains(4));
+    }
+
+    @Test
+    public void containsFalseTest(){
+        SinglyLinkedList list = new SinglyLinkedList();
+        list.add(4);
+        list.add(8);
+        list.add(9);
+        Assert.assertFalse(list.contains(5));
+    }
+
+    @Test
+    public void findTest(){
+        SinglyLinkedList list = new SinglyLinkedList();
+        Integer expected = 2;
+        list.add(4);
+        list.add(8);
+        list.add(9);
+        Integer actual = list.find(9);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void findFalseTest(){
+        SinglyLinkedList list = new SinglyLinkedList();
+        Integer expected = 2;
+        list.add(4);
+        list.add(8);
+        list.add(9);
+        Integer actual = list.find(12);
+        Assert.assertNotEquals(expected, actual);
+    }
+
+    @Test
+    public void copyTest(){
+        SinglyLinkedList list = new SinglyLinkedList();
+        list.add(4);
+        list.add(8);
+        list.add(9);
+        SinglyLinkedList actual = list.copy();
+        Assert.assertEquals(list.get(1), actual.get(1));
+    }
+
+    @Test
+    public void copyFalseTest(){
+        SinglyLinkedList list = new SinglyLinkedList();
+        list.add(4);
+        list.add(8);
+        list.add(9);
+        SinglyLinkedList actual = list.copy();
+        list.remove(1);
+        Assert.assertNotEquals(list.get(1), actual.get(1));
+    }
+
+    @Test
+    public void sortTest(){
+        SinglyLinkedList list = new SinglyLinkedList();
+        Integer actual = 9;
+        list.add(4);
+        list.add(9);
+        list.add(8);
+        SinglyLinkedList sortList = list.sort();
+        Assert.assertEquals(sortList.get(2), actual);
+    }
 }
